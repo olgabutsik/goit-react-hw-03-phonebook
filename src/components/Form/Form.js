0b55aa7d0fe37a9450465cb8a89cc2ed7adb7468
Olha <cukbu30@gmai.com>
2023-02-20 14:32:28 +0200

    fixed

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,6 @@
 import { Component } from 'react';
-
 import { FormStyled, FormInput, FormLabel, FormButton } from './Form.styled';
 import PropTypes from 'prop-types';
-
 export class Form extends Component {
   state = {
     name: '',
