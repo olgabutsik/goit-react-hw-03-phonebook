6d9260edc7366be44699787360212c52cd3989e4
Olha <cukbu30@gmai.com>
2023-02-20 14:45:10 +0200

    Update storage.js

--- src/components/helpers/storage.js
+++ src/components/helpers/storage.js
@@ -1,6 +1,7 @@
 const save = (key, value) => {
   try {
     const serializedState = JSON.stringify(value);
+
     window.localStorage.setItem(key, serializedState);
   } catch (error) {
     console.error('Set state error: ', error.message);
